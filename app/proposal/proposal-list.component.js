f8991a9ca9786c32fa70a96139adac5d83a47b87
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-03-06 21:37:35 -0500

    added token proposal and displayed them in the html

--- app/proposal/proposal-list.component.js
+++ app/proposal/proposal-list.component.js
@@ -9,8 +9,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var proposal_1 = require('./proposal');
 var ProposalListComponent = (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, 'Abc Company', 'http://portfolio1.jordanhudgens.com', 'Ruby on Rails1', 151, 121, 15.1, 'jordan1@devcamp.com');
+        this.proposalTwo = new proposal_1.Proposal(12, 'Xyz Company', 'http://portfolio2.jordanhudgens.com', 'Ruby on Rails2', 152, 122, 15.2, 'jordan2@devcamp.com');
+        this.proposalThree = new proposal_1.Proposal(1, 'DEF Company', 'http://portfolio3.jordanhudgens.com', 'Ruby on Rails3', 153, 123, 15.3, 'jordan3@devcamp.com');
+        this.proposals = [
+            this.proposalOne, this.proposalTwo, this.proposalThree
+        ];
     }
     ProposalListComponent = __decorate([
         core_1.Component({
--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
     moduleId: module.id,
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
     templateUrl: 'proposal-list.component.html'
 })
 
-export class ProposalListComponent {}
+export class ProposalListComponent {
+    proposalOne: Proposal = new Proposal(15, 'Abc Company','http://portfolio1.jordanhudgens.com', 
+    'Ruby on Rails1', 151, 121, 15.1, 'jordan1@devcamp.com')
+    proposalTwo: Proposal = new Proposal(12, 'Xyz Company','http://portfolio2.jordanhudgens.com', 
+    'Ruby on Rails2', 152, 122, 15.2, 'jordan2@devcamp.com')
+    proposalThree: Proposal = new Proposal(1, 'DEF Company','http://portfolio3.jordanhudgens.com', 
+    'Ruby on Rails3', 153, 123, 15.3, 'jordan3@devcamp.com')
+    proposals: Proposal[] = [
+        this.proposalOne, this.proposalTwo, this.proposalThree
+    ]
+}
