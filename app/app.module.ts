b448c97c6772d016a2f0541300fa6c59745e8c6a
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-03-09 21:22:49 -0500

    Barebone proposal form done

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -12,7 +14,8 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		FormsModule
 	],
 	declarations: [
 		AppComponent,
