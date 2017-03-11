24ff6a4060987eb3ad3f769413701083a862de99
Adam Gauvreau <adamgauvreau@hotmail.com>
2017-03-11 14:20:46 -0500

    proposal show page per ID

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,6 +8,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent} from './proposal/proposal-list.component';
 import { ProposalNewComponent }	from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -23,6 +24,7 @@ import { AppRoutingModule } from './app-routing.module';
 		DocumentsComponent,
 		ProposalListComponent,
 		ProposalNewComponent,
+		ProposalShowComponent,
 	],
 	bootstrap: [
 		AppComponent
